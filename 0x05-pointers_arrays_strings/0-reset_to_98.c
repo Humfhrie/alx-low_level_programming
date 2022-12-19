a9cafa8b968c1a051fe3b93e4ad69b6955b9a9e4
humfhrie <humpheryalph@gmail.com>
2022-12-19 14:44:13 -0800

    Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer as parameter and updates the value it points
+ * @n: int to check
+ *
+ * Return: 0 is success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
