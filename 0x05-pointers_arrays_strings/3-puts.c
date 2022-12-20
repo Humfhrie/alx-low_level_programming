f6efdf5f54cb611bfbbce6176a32a65a7c771b24
humfhrie <humpheryalph@gmail.com>
2022-12-19 16:10:19 -0800

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _puts -  function that prints a string
+ * _puts - function that prints a string
  * @str: the string
  * 
  * Return: the length of the string
@@ -8,7 +8,9 @@
 void _puts(char *str)
 {
 	int i = 0;
-	whle (str[i] != '\0')
+
+	while (str[i] != '\0')
+
 	{
 		_putchar(str[I]);
 		i++;
