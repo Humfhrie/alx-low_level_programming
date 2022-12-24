23dcde87632b32b085de986114750a0289a5d851
Humfhrie <114100121+Humfhrie@users.noreply.github.com>
2022-12-24 05:09:49 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,20 +5,10 @@
  * _strncat - function that concatenates two strings.
  * @dest: first parameter
  * @src: second parameter
- * Return: A string
+ * Return: a string
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int len = 0, i;
-
-	while (dest[len])
-		len++
-	
-	for (i = 0; src[i] != 0; i++)
-	{
-		dest[len] = src[i];
-		len+= 1;
-	}
-	dest[len] = '\0';
+	strcat(dest, src);
 	return (dest);
 }
