1d3360cd8ea7bd36cb6634ce66647d04549afbc7
humfhrie <humpheryalph@gmail.com>
2022-12-23 20:22:19 -0800

    4-rev_array

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,10 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _strncat - function that concatenates two strings.
- * @dest: first parameter
+ * @dest: first paaramter, that concatenates two strings.
  * @src: second parameter
- * 
  * Return: A string
  */
 char *_strncat(char *dest, char *src, int n)
@@ -13,11 +13,10 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (dest[len])
 		len++
-	
 	for (i = 0; src[i] != 0; i++)
 	{
 		dest[len] = src[i];
-		len+= 1;
+		len += 1;
 	}
 	dest[len] = '\0';
 	return (dest);
