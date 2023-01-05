77280f91498072e9c986a5fe6ab0c76e59bacdb5
humfhrie <humpheryalph@gmail.com>
2023-01-04 19:06:41 -0800

    Inception. Now, before you bother telling me it's impossible

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,33 +1,24 @@
 #include "main.h"
 
 /**
- * str_checker - check if two strings are identical.
- * @s1: string_1 base address.
- * @s2: string_2 base address.
- * @i: left index.
- * @j: special index. 
- * Return: 1 if s is palindrome, 0 otherwise.
- */
-int str_checker(char *s1, char *s2, int i, int j)
-{
-	if (s1[i] == '\0' && s2[j] == '\0')
-		return (1);
-	if (s1[i] == s2[j])
-		return (str_checker(s1, s2, i + 1, j + 1));
-	if (s1[i] == '\0' && s2[j] == '*')
-		return (str_checker(s1, s2, i, j + 1));
-	if (s2[j] == '*')
-		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
-	return (0);
-}
-/**
- * wildcmp - check if strings could be considered identical
- * @s1: base address for string.
- * @s2: base address for string.
+ * wildcmp - Compare two strings allowing for wildcard
+ * @s1: String to be compared
+ * @s2: String to be compared against
  *
- * Return: 1 if are considered identical.
+ * Return: 1 if considered identical, 0 otherwise
  */
 int wildcmp(char *s1, char *s2)
 {
-	return (str_checker(s1, s2, 0, 0));
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
 }
