1db60dfbf25fd6833e7581481d9b150800ea16eb
Humfhrie <114100121+Humfhrie@users.noreply.github.com>
2023-01-07 04:32:15 +0100

    Update 0-strcat.c

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,11 +1,13 @@
 #include "main.h"
- /**
- * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
- *
- * Return: void
+#include <string.h>
+
+/**
+ * _strncat - function that concatenates two strings.
+ * @dest: first paaramter, that concatenates two strings.
+ * @src: second parameter
+ * Return: a string
  */
+
 char *_strcat(char *dest, char *src)
 {
 	int i;
@@ -23,7 +25,6 @@ char *_strcat(char *dest, char *src)
 		i++;
 		j++;
 	}
-
-	dest[i] = '\0';
+	dest[i] '\0';
 	return (dest);
 }
