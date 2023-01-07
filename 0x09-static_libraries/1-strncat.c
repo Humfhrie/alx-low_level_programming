a45e97ce65f553ac09ab036e63cdf4f2c50e3914
Humfhrie <114100121+Humfhrie@users.noreply.github.com>
2023-01-07 04:54:43 +0100

    Update 1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,30 +1,26 @@
 #include "main.h"
-/**
- * _strncat - concatenate two strings
- * using at most n bytes from src
- * @dest: input value
- * @src: input value
- * @n: input value
- *
- * Return: dest
- */
-char *_strncat(char *dest, char *src, int n)
-{
-	int i;
-	int j;
 
-	i = 0;
-	while (dest[i] != '\0')
+/**
+ * _memcpy - function copies @n bytes from memory area @src
+ * to memory area @dest
+ *
+ * @n: function copies
+ *
+ * @src: bytes from memory area
+ *
+ * @dest: to memory area
+ *
+ * Return: a pointer to @dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
 	{
+		dest[i] = src[i];
 		i++;
 	}
-	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-	dest[i] = src[j];
-	i++;
-	j++;
-	}
-	dest[i] = '\0';
+
 	return (dest);
 }
