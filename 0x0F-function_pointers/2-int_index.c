c83ef15c4259b26cf144b66dc82389b6db4ebf8c
humfhrie <humpheryalph@gmail.com>
2023-01-18 19:08:59 -0800

    int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,11 +12,21 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i = 0;
 
-	if (array && cmp)
+	if (size > 0)
 	{
-		for (i = 0; i < size; i++)
-			return (i);
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+
+				i++;
+			}
+		}
 	}
+
+	return (-1);
 }
